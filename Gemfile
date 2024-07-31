d4edfeccca6d7a884b4b49a2c30d537ec2d628aa
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-31 20:12:38 +0900

    Updated recipe model to update content after a new save to the DB

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ gem "sassc-rails"
 
 # Manually added gems
 gem "ruby-openai"
+gem "cloudinary"
 
 group :development, :test do
   gem "dotenv-rails"
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,20 +1,35 @@
 class Recipe < ApplicationRecord
-  # def content
-  #  Rails cache.fetch will store the value calculated so that when you reload/refresh it doesnt have to make another request. Instead it keeps the value calculated previously
-  #  The value will be stored until time expires or there is a change to the object key
-  #   Rails.cache.fetch("#{cache_key_with_version}/content") do
-  #     # Create a new openai client
-  #     client = OpenAI::Client.new
+  # after a save is made, if the name or ingredients have changed we get new content from chatgpt to upadte our recipe object
+  after_save :set_content, if: -> { saved_change_to_name? || saved_change_to_ingredients? }
 
-  #     # make a new prompt to openai to get a response back from chatgpt
-  #     response = client.chat(parameters: {
-  #       model: "gpt-3.5-turbo",
-  #       messages: [{
-  #         role: "user",
-  #         content: "Make a simple recipe for #{name} with ingredients #{ingredients}"
-  #       }]
-  #     })
-  #     return response["choices"][0]["message"]["content"]
-  #   end
-  # end
+  # We only load content from chatgpt if the content is blank.
+  # Otherwise we just load the static string that is already stored in the database
+  def content
+    if super.blank? #if the content normally gives you nothing
+      set_content
+    else
+      super
+    end
+  end
+
+  private
+
+  def set_content
+    # Create a new openai client
+    client = OpenAI::Client.new
+
+    # make a new prompt to openai to get a response back from chatgpt
+    response = client.chat(parameters: {
+      model: "gpt-3.5-turbo",
+      messages: [{
+        role: "user",
+        content: "Make a simple recipe for #{name} with ingredients #{ingredients}, format the instructions into an organized list and dont list the ingredients"
+      }]
+    })
+
+    # Getting the content from chatgpt and updating the content for the recipe object
+    new_content = response["choices"][0]["message"]["content"]
+    update(content: new_content)
+    return new_content
+  end
 end
