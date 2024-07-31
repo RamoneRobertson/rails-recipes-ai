cae6e2d60725950906fb5a8ec8b996ec1bcb87d5
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-31 21:02:23 +0900

    Can now generate images for a recipe with chatgpt

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,11 @@
+require "open-uri"
 class Recipe < ApplicationRecord
   # after a save is made, if the name or ingredients have changed we get new content from chatgpt to upadte our recipe object
-  after_save :set_content, if: -> { saved_change_to_name? || saved_change_to_ingredients? }
+  after_save if: -> { saved_change_to_name? || saved_change_to_ingredients? } do
+    set_content
+    set_photo
+  end
+
   has_one_attached :photo
 
   # We only load content from chatgpt if the content is blank.
@@ -32,5 +37,27 @@ class Recipe < ApplicationRecord
     new_content = response["choices"][0]["message"]["content"]
     update(content: new_content)
     return new_content
+  end
+
+  def set_photo
+    client = OpenAI::Client.new
+    response = client.images.generate(parameters: {
+      prompt: "Give me an image of a recipe for #{name} with ingredients #{ingredients}",
+      size: "256x256"
+    })
+    id = response["data"][0]["url"]
+
+    # This is the url given by chatgpt from the prompt
+    url =  response["data"][0]["url"]
+
+    # In order to attach the photo to the recipe we must open it from the internet
+    file = URI.open(url)
+
+    # If a photo already exists we delete it and atach the new photo
+    photo.purge if photo.attached?
+
+    # Now we send the file to cloudinary and attach it to the recipe
+    photo.attach(io: file, filename: "#{name}.png", content_type: "image/png")
+    return photo
   end
 end
